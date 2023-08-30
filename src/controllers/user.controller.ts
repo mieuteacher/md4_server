9dce1999fbce13437ec3a6a0b5d3691cfe386c62
Mieu Teacher <mieuteacher@gmail.com>
2023-08-30 17:59:43 +0700

    update

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import userModel, {NewUser} from "../models/user.model";
 import { Request, Response } from "express";
 import Text from '../text'
+import mail, {templates} from "../services/mail";
 export default {
     register: async function(req: Request, res: Response) {
        try {
@@ -12,6 +13,21 @@ export default {
             let modelRes = await userModel.register(newUser);
 
             modelRes.message = (Text(String(req.headers.language)) as any)[modelRes.message];
+
+            /* Mail */
+            if(modelRes.status) {
+                mail.sendMail({
+                    to: `${modelRes.data?.email}`,
+                    subject: "Xác thực email",
+                    html: templates.emailConfirm({
+                        confirmLink: "chuaco",
+                        language: String(req.headers.language),
+                        productName: "Miêu Store",
+                        productWebUrl: "abc.com",
+                        receiverName: modelRes.data?.firstName + '' + modelRes.data?.lastName
+                    })
+                })
+            }
 
             return res.status(modelRes.status ? 200 : 213).json(modelRes);
        }catch(err) {
